b88047268ff36bb774900ca322961fd98368422e
Yaseen Amin <yaseenaminm@gmail.com>
2025-09-25 18:09:55 +0500

    016 Rendering the Items List

--- 05-travel-list/starter/travel-list/src/App.js
+++ 05-travel-list/starter/travel-list/src/App.js
@@ -1,5 +1,10 @@
 import React from "react";
 
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
+
 export default function App() {
   return (
     <div className="app">
@@ -22,8 +27,36 @@ function Form() {
   );
 }
 function PackingList() {
-  return <div className="list">LIST</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => {
+          return <Item item={item} />;
+        })}
+      </ul>
+    </div>
+  );
 }
+
+function Item({ item }) {
+  return (
+    <li>
+      <span
+        style={
+          item.packed
+            ? {
+                textDecoration: "line-through",
+              }
+            : {}
+        }
+      >
+        {item.quantity} {item.description}
+      </span>
+      <span>❌</span>
+    </li>
+  );
+}
+
 function Status() {
   return (
     <footer className="stats">
