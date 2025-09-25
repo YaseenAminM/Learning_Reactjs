2041d7cdffe7379df13fd46efccb801f3ca79c49
Yaseen Amin <yaseenaminm@gmail.com>
2025-09-25 16:04:57 +0500

    008 Adding Another Piece of State

--- 05-travel-list/starter/steps/src/App.js
+++ 05-travel-list/starter/steps/src/App.js
@@ -9,9 +9,7 @@ const messages = [
 export default function App() {
   // State
   const [step, setStep] = useState(1);
-  const [test, setTest] = useState({
-    name: "Yaseen",
-  });
+  const [isOpen, setIsOpen] = useState(true);
 
   // Handler Function
   function handlePrevious() {
@@ -25,50 +23,50 @@ export default function App() {
     if (step < 3) {
       setStep((step) => step + 1);
     }
+  }
 
-    // BAD PRACTICE
-    // test.name = "Ali";
-
-    // GOOD PRACTICE
-    setTest({
-      name: "Ali",
-    });
+  function handlerIsOpen() {
+    setIsOpen(!isOpen);
   }
 
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={`${step >= 1 ? "active" : null}`}>1</div>
-        <div className={`${step >= 2 ? "active" : null}`}>2</div>
-        <div className={`${step >= 3 ? "active" : null}`}>3</div>
-      </div>
+    <>
+      <button className="close" onClick={handlerIsOpen}>
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={`${step >= 1 ? "active" : null}`}>1</div>
+            <div className={`${step >= 2 ? "active" : null}`}>2</div>
+            <div className={`${step >= 3 ? "active" : null}`}>3</div>
+          </div>
 
-      <p className="message">
-        Step {messages[step - 1]}
-        {test.name}
-      </p>
+          <p className="message">Step {messages[step - 1]}</p>
 
-      <div className="buttons">
-        <button
-          style={{
-            color: "#fff",
-            backgroundColor: "#7950f2",
-          }}
-          // Mouse Click Event
-          onClick={handlePrevious}
-        >
-          Previous
-        </button>
-        <button
-          style={{
-            color: "#fff",
-            backgroundColor: "#7950f2",
-          }}
-          onClick={handleNext}
-        >
-          Next
-        </button>
-      </div>
-    </div>
+          <div className="buttons">
+            <button
+              style={{
+                color: "#fff",
+                backgroundColor: "#7950f2",
+              }}
+              // Mouse Click Event
+              onClick={handlePrevious}
+            >
+              Previous
+            </button>
+            <button
+              style={{
+                color: "#fff",
+                backgroundColor: "#7950f2",
+              }}
+              onClick={handleNext}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
