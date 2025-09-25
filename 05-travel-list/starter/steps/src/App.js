e0516a15e7e890aa69b83574e3828d4ce59e6b55
Yaseen Amin <yaseenaminm@gmail.com>
2025-09-25 14:58:30 +0500

    005 Creating a State Variable With useState

--- 05-travel-list/starter/steps/src/App.js
+++ 05-travel-list/starter/steps/src/App.js
@@ -1,26 +1,35 @@
+import { useState } from "react";
+
 const messages = [
   "Learn React ⚛️",
   "Apply for jobs 💼",
   "Invest your new income 🤑",
 ];
+
 export default function App() {
-  // Variable
-  const step = 1;
+  // State
+  let [step, setStep] = useState(1);
 
   // Handler Function
   function handlePrevious() {
-    alert("Previous");
+    // Updating the State
+    if (step > 1) {
+      setStep((step) => step - 1);
+    }
   }
   function handleNext() {
-    alert("Previous");
+    // Updating the State
+    if (step < 3) {
+      setStep((step) => step + 1);
+    }
   }
 
   return (
     <div className="steps">
       <div className="numbers">
-        <div className={`${step === 1 ? "active" : null}`}>1</div>
-        <div className={`${step === 2 ? "active" : null}`}>2</div>
-        <div className={`${step === 3 ? "active" : null}`}>3</div>
+        <div className={`${step >= 1 ? "active" : null}`}>1</div>
+        <div className={`${step >= 2 ? "active" : null}`}>2</div>
+        <div className={`${step >= 3 ? "active" : null}`}>3</div>
       </div>
 
       <p className="message">Step {messages[step - 1]}</p>
@@ -41,6 +50,7 @@ export default function App() {
             color: "#fff",
             backgroundColor: "#7950f2",
           }}
+          onClick={handleNext}
         >
           Next
         </button>
